d358067d601f7254f1eead47714a4f46e624d94d
Zfttter <ZHR1778990@outlook.com>
2025-02-20 21:51:41 -0500

    react version update

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -10,12 +10,16 @@ import {
   Button,
   InputGroup,
 } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const HeaderComponent = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand href="/">BEST ONLINE SHOP</Navbar.Brand>
+        </LinkContainer>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
